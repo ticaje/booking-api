9465bf8035ccf54966bc4b027cc0230fd78b21ec
Hector Luis Barrientos Margolles <hbarrientos@eshopworld.com>
2021-07-01 14:22:01 +0200

    Add missing test(domain command)

---
+++ tests/Unit/Domain/Calendar/Disabling/CQRS/CommandTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+/**
+ * Domain Test
+ * @package Ticaje_BookingApi
+ * @author  Hector Luis Barrientos <ticaje@filetea.me>
+ */
+
+namespace Ticaje\BookingApi\Test\Unit\Domain\Calendar\Disabling\CQRS;
+
+use Ticaje\BookingApi\Domain\Policies\Calendar\Disabling\CQRS\Command;
+use Ticaje\BookingApi\Domain\Policies\Calendar\Disabling\CQRS\CommandSignature;
+use Ticaje\BookingApi\Test\Unit\BaseTest as ParentClass;
+use Ticaje\BookingApi\Test\Unit\Traits\ComplexConstructor;
+
+/**
+ * Class CommandTest
+ * @package Ticaje\BookingApi\Test\Unit\Domain\Calendar\Disabling\CQRS
+ */
+class CommandTest extends ParentClass
+{
+    use ComplexConstructor;
+
+    protected $interface = CommandSignature::class;
+
+    protected $class = Command::class;
+
+    public function testExtractType()
+    {
+        $expected = [];
+        $this->instance->method('extractType')->willReturn($expected);
+        $methodCallResponse = $this->instance->extractType([]);
+        $this->assertTrue(is_array($methodCallResponse), $methodCallResponse, 'Assert returns array');
+    }
+}
